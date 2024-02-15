68cf27ab25df29061a94969f8ce6a0fa0e5f8b30
Ryan Hemlock <ryan@ryanhemlock.com>
2024-02-15 10:19:33 -0500

    Changed netlify link

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
+// we need to npm i cors
+const cors = require('cors');
 const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
 const PORT = process.env.PORT || 3001;
-const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
-
+const app = express();
+// to avoid referrer issues we use this middleware
+app.use((req, res, next) => {
+  res.setHeader('Referrer-Policy', 'no-referrer');
+  next();
+});
+// setting options for cors origins
+const corsOptions = {
+  origin: 'https://teamsync-client-project-3.netlify.app', // Adjust this to your client's URL on netlify with no / at the end
+  credentials: false, // we aren't tossing cookies around
+  methods: ['GET', 'POST', 'OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // this allows auth headers to get passed from client to server this is the `authorization bearer token` stuff
+};
+app.use(cors(corsOptions));
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
-
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-
+  // add cors false to this middleware
   app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
+    context: authMiddleware,
+    cors: false
   }));
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
-
+  // delete the production stuff AND delete your .npmrc
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -40,6 +43,5 @@ const startApolloServer = async () => {
     });
   });
 };
-
 // Call the async function to start the server
 startApolloServer();
